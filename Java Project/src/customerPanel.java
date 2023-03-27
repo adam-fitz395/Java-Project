0ac349404d6d27627b975049f3599a32767daa37
adam_fitz395 <113912436+adam-fitz395@users.noreply.github.com>
2023-03-27 21:03:20 +0100

    Update

--- Java Project/src/customerPanel.java
+++ Java Project/src/customerPanel.java
@@ -9,20 +9,24 @@ public class customerPanel
     // Labels
     private static JLabel custNameLabel;
     private static JLabel custSurnameLabel;
+    private static JLabel custDOBLabel;
     
     // Textfields
     private static JTextField custNameTfield;
     private static JTextField custSurnameTfield;
+    private static JTextField custDOBTfield;
 
     // Buttons
     private static JButton okButton;
     private static JButton custPanelButton;
+    private static JButton invoicePanelButton;
     public static void main(String[] args) 
     {
         // Create Customer frame and panel
         JFrame custFrame = new JFrame("Insert Customer");
         JPanel custPanel = new JPanel();
-        custFrame.setSize(400,300);
+        custFrame.setSize(600,600);
+        custPanel.setSize(600,600);
         custFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         custFrame.add(custPanel);
         custPanel.setLayout(null);
@@ -32,23 +36,37 @@ public class customerPanel
         custNameLabel = new JLabel("Name:");
         custNameLabel.setBounds(10, 40, 75, 25);
         custPanel.add(custNameLabel);
+        custNameLabel.setVisible(true);
 
         custNameTfield = new JTextField();
         custNameTfield.setBounds(90, 40, 100, 25);
         custPanel.add(custNameTfield);
+        custNameTfield.setVisible(true);
 
         custSurnameLabel = new JLabel("Surname:");
         custSurnameLabel.setBounds(10, 70, 75, 25);
         custPanel.add(custSurnameLabel);
+        custSurnameLabel.setVisible(true);
 
         custSurnameTfield = new JTextField();
         custSurnameTfield.setBounds(90, 70, 100, 25);
         custPanel.add(custSurnameTfield);
+        custSurnameTfield.setVisible(true);
+
+        custDOBLabel = new JLabel("DOB (YYYY-MM-DD):");
+        custDOBLabel.setBounds(10, 100, 75, 25);
+        custPanel.add(custDOBLabel);
+        custDOBLabel.setVisible(true);
+
+        custDOBTfield = new JTextField();
+        custDOBTfield.setBounds(90, 100, 100, 25);
+        custPanel.add(custDOBTfield);
+        custDOBTfield.setVisible(true);
 
         // Add buttons
         // OK button
         okButton = new JButton("OK");
-        okButton.setBounds(10, 100, 60, 25);
+        okButton.setBounds(10, 130, 60, 25);
         okButton.addActionListener(new ActionListener () 
         {
             public void actionPerformed (ActionEvent e)
@@ -63,15 +81,17 @@ public class customerPanel
             }
         });
         custPanel.add(okButton);
+        okButton.setVisible(true);
 
         // Customer Panel Button
         custPanelButton = new JButton("Customer");
         custPanelButton.setBounds(10, 10, 100, 25);
         custPanel.add(custPanelButton);
+        custPanelButton.setVisible(true);
 
-        custPanelButton = new JButton("Invoice");
-        custPanelButton.setBounds(10, 10, 100, 25);
-        custPanel.add(custPanelButton);
+        invoicePanelButton = new JButton("Invoice");
+        invoicePanelButton.setBounds(120, 10, 80, 25);
+        custPanel.add(invoicePanelButton);
 
     }
 
